553ed0b05474c44f656fb4ef7efced4c29c89eb5
Aleksey <aleshabelov2001@mail.ru>
2023-08-29 01:46:03 +0400

    Добавлена функция ODE_newton, реализующая решение ДУ методом Ньютона (одномерный случай)

--- numerical_methods/numerical_methods.cpp
+++ numerical_methods/numerical_methods.cpp
@@ -2,19 +2,84 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+double f(double x) {
+    return x * x;
+}
+
+double* ff(double* u, float t) {
+    double phi = u[0];
+    double omega = u[1];
+    double g = 9.81; double l = 1;
+
+    double res[2] = {0., 0.};
+    res[0] = omega;
+    res[1] = -g / l * phi;
+    return res;
+}
+
+
+void print_to_txt(vector <double> t, vector <double> res) {
+    // Функция для печати массивов в .txt. Сначала в стоблец вектор t, затем res
+    std::ofstream out;          // поток для записи
+    out.open("hello.txt");      // открываем файл для записи
+    if (out.is_open())
+    {
+        for (int i = 0; i < t.size(); i++) {
+            out << t[i] << "\t" << res[i] << std::endl;
+        }
+    }
+    out.close();
+    std::cout << "File has been written" << std::endl;
+}
+
+struct ODE_res {
+    // Структура для возврата результа интегирования функции ODE_newton
+    vector <double> t;
+    vector <double> u;
+};
+
+ODE_res ODE_newton(double(*f)(double x), double u0, double t0, double T) {
+    // Функция решает линейное дифференциальное уравнение методом Ньютона 
+    // Входные параметры:
+    // f - функция, возвращающая праву часть
+    // u0  - начальное условие
+    // t0, T - время интегрирования
+    ODE_res res;
+
+    int N_MAX = 200;
+    double dt = (T - t0) / static_cast <double>(N_MAX);
+
+    res.t.push_back(t0);
+    res.u.push_back(u0);
+
+    double next_u = u0;
+    double next_t = t0;
+    for (int i = 1; i < N_MAX + 1; i++) {
+        next_u = res.u[i - 1] + dt * f(res.t[i - 1]);
+        next_t = next_t + dt;
+        res.u.push_back(next_u);
+        res.t.push_back(next_t);
+    }
+    return res;    
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ODE_res res;
+    res = ODE_newton(f, 1, 0, 30);
+    
+    print_to_txt(res.t, res.u);
+ 
+    
+ 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
