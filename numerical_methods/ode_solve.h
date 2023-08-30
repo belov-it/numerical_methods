ba366d87152c43b6db3f0f28b340f1e4a7bafe4b
Aleksey <aleshabelov2001@mail.ru>
2023-08-30 23:08:09 +0400

    Изменил комменты на англ

--- numerical_methods/ode_solve.h
+++ numerical_methods/ode_solve.h
@@ -6,13 +6,13 @@ using namespace std;
 using namespace Eigen;
 
 struct ODE_res {
-    // Структура для возврата результа интегирования функции ODE_newton
+    // Structure for returning the result 1D of the ODE solver
     vector <double> t;
     vector <double> u;
 };
 
 struct ODE_res_2D {
-    // Структура для возврата результа интегирования функции ODE_newton
+    // Structure for returning the result 1D of the ODE solver
     Matrix <float, 100, 1> t;
     Matrix <float, 100, 2> u;
 };
@@ -22,16 +22,16 @@ struct ODE_res_2D {
 ODE_res_2D ODE_newton_2D(Matrix <float, 2, 1>(*f)(Matrix <float, 2, 1> x), Matrix <float, 2, 1> u0, float t0, float T) {
     ODE_res_2D res;
 
-    int N = 100; // Количество итераций
+    int N = 100; // Count of iteration
     float dt = (T - t0) / static_cast <float> (N);
 
-    // Добавляем в итоговые массивы начальные значения
+    // Add initial condition
     res.t(0) = t0;
     for (int i = 0; i < 2; i++) {
         res.u(0, i) = u0(i);
     }
 
-    // Для хранения результата функции
+    // Temporary storage of function result
     Matrix <float, 2, 1> f_tmp;
 
     for (int m = 1; m < N; m++) {
@@ -47,11 +47,11 @@ ODE_res_2D ODE_newton_2D(Matrix <float, 2, 1>(*f)(Matrix <float, 2, 1> x), Matri
 }
 
 ODE_res ODE_newton(double(*f)(double x), double u0, double t0, double T) {
-    // Функция решает линейное дифференциальное уравнение методом Ньютона 
-    // Входные параметры:
-    // f - функция, возвращающая праву часть
-    // u0  - начальное условие
-    // t0, T - время интегрирования
+    // Solve 1D ODE using newton method
+    // Params:
+    // f - Right-hand side of the differential equation
+    // u0  - Initial condition
+    // t0, T - Integration time
     ODE_res res;
 
     int N_MAX = 200;
